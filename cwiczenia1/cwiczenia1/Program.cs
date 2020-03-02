a0d722960022221813ba3315866b12a6dd9a71b3
s19605 <s19605@pjwstk.edu.pl>
2020-03-02 11:35:56 +0100

    dodano plik

---
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
